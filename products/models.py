c577336e721f34640fbd27c40fb685bbdf971863
Sibghat-Omar <sibghatullah>
2025-05-18 11:32:12 +0430

    products models added

--- products/models.py
+++ products/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    parent = models.ForeignKey('self', blank=True, null=True, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    avatar = models.ImageField(blank=True, upload_to='categories/')
+    is_enable = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    
+    class Meta:
+        db_table = 'categories'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+    
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    avatar = models.ImageField(blank=True, upload_to='products/')
+    category = models.ManyToManyField(Category, blank=True)
+    is_enable = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'products'
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+class file(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    file = models.FileField(upload_to='files/%Y/%m/%d/')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True, null=True)
+    is_enable = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'files'
+        verbose_name = 'File'
+        verbose_name_plural = 'Files'
